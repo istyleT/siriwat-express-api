7392c328ff708bff45b951fae5777738c85d2df8
I-style T <ajantug@gmail.com>
2024-09-09 16:21:52 +0700

    Refactor console log statement in userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -156,7 +156,7 @@ userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
       this.passwordChangedAt.getTime() / 1000,
       10
     );
-    console.log(this.passwordChangedAt, JWTTimestamp);
+    // console.log(this.passwordChangedAt, JWTTimestamp);
     return JWTTimestamp < changedTimestamp; // 100 < 200
   }
   // False means NOT changed
