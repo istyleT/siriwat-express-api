4c21adb64b285545a11e9784653b46712c7cfb6f
I-style T <ajantug@gmail.com>
2026-01-04 22:42:03 +0700

    refactor: streamline swpartkitSchema definition; remove unused fields and simplify structure

--- models/siriwatModel/swpartkitModel.js
+++ models/siriwatModel/swpartkitModel.js
@@ -1,47 +1,37 @@
 const mongoose = require("mongoose");
-const moment = require("moment-timezone");
 
-const swpartkitSchema = new mongoose.Schema({
-  code: {
-    type: String,
-    unique: true,
-    required: [true, "กรุณาระบุรหัสชุด Kit"],
-  },
-  description: {
-    type: String,
-    unique: true,
-    required: [true, "กรุณาระบุชื่อชุด Kit"],
-  },
-  items: {
-    type: [
-      {
-        part: { type: mongoose.Schema.Types.ObjectId, ref: "Pricelist" },
-        qty: {
-          type: Number,
-          default: 1,
+const swpartkitSchema = new mongoose.Schema(
+  {
+    code: {
+      type: String,
+      unique: true,
+      required: [true, "กรุณาระบุรหัสชุด Kit"],
+    },
+    description: {
+      type: String,
+      unique: true,
+      required: [true, "กรุณาระบุชื่อชุด Kit"],
+    },
+    items: {
+      type: [
+        {
+          part: { type: mongoose.Schema.Types.ObjectId, ref: "Pricelist" },
+          qty: {
+            type: Number,
+            default: 1,
+          },
         },
-      },
-    ],
-    default: [],
+      ],
+      default: [],
+    },
   },
-  //field พื้นฐาน
-  created_at: {
-    type: Date,
-    default: () => moment.tz(Date.now(), "Asia/Bangkok").toDate(),
-  },
-  updated_at: {
-    type: Date,
-    default: null,
-  },
-  canceled_at: {
-    type: Date,
-    default: null,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 //create index
-swpartkitSchema.index({ code: 1 });
-swpartkitSchema.index({ description: 1 });
+swpartkitSchema.index({ code: 1, description: 1 });
 
 swpartkitSchema.pre(/^find/, function (next) {
   this.populate({
